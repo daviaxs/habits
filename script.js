b355b90661f263cc4a9c0c2f2b1d69d37ac9a836
Davi Alves <cadwvk@gmail.com>
2023-01-19 18:00:39 -0300

    bug fix - responsive css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 
 const data = {
-  exercise: ["01-01", "01-02", "01-03", "01-04", "01-07", "01-09"],
+  exercise: ["01-01", "01-02", "01-03", "01-04", "01-07", "01-09", "01-10", "01-11", "01-12", "01-13", "01-14", "01-15", "01-16", "01-17", "01-18"],
   hydrate: ["01-01", "01-04", "01-08"]
 }
 
