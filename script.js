a92acfc0e4d0521bac07bc013574d65065bea9c6
Davi Alves <cadwvk@gmail.com>
2023-01-19 20:18:40 -0300

    add system get day js and modify scrollbar css

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('.button-add')
 
-const data = {
-  exercise: ["01-01", "01-02", "01-03", "01-04", "01-07", "01-09", "01-10", "01-11", "01-12", "01-13", "01-14", "01-15", "01-16", "01-17", "01-18"],
-  hydrate: ["01-01", "01-04", "01-08"]
+button.addEventListener('click', add)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert('🔴 Dia já incluso 🔴')
+    return  }
+
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+// const data = {
+//   exercise: ["01-01", "01-02", "01-03", "01-04", "01-07", "01-09", "01-10", "01-11", "01-12", "01-13", "01-14", "01-15", "01-16", "01-17", "01-18"],
+//   hydrate: ["01-01", "01-04", "01-08"]
+// }
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
 
 // sistema Switch
 const html = document.documentElement
